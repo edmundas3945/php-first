2659c30a81db9574bbbc386f69cc95d2a8218398
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2020-12-09 14:34:45 +0200

    12.9 korteles ir termometras

--- index.php
+++ index.php
@@ -2,86 +2,334 @@
 error_reporting(E_ALL);
 error_reporting(-1);
 ini_set('error_reporting', E_ALL);
-date_default_timezone_set ('Europe/Vilnius');
+date_default_timezone_set('Europe/Vilnius');
 
 //$page = $_SERVER['PHP_SELF'];
 //$sec = "1";
 //header("Refresh: $sec; url=$page");
 
-//$day1 = rand(0,5);
-//$day2 = rand(0,5);
-//$day3 = rand(0,5);
-//$day4 = rand(3,7);
-//$day5 = rand(5,15);
-//$day6 = rand(2,4);
-//$day7 = rand(0,2);
-$month = 0;
-$dalmuo = 0;
+$klase = ['jaunikis Jonas', 'smaikscioji Lazauskiene', 'saules sunus'];
 
-for($m = 1; $m <= 4; $m++){
-    $day1 = rand(0,5);
-    $day2 = rand(0,5);
-    $day3 = rand(0,5);
-    $day4 = rand(3,7);
-    $day5 = rand(5,15);
-    $day6 = rand(2,4);
-    $day7 = rand(0,2);
 
-    $week = $day1 + $day2 +$day3 + $day4 + $day5 + $day6 + $day7;
-    $month += $week;
-}
-$modalOfMonth = $month % 20;
-$dalmuo = ($month - $modalOfMonth)/ 20;
+$vartotojai = [
+    [
+        'vardas' => 'Linute',
+        'pavarde' => 'Kudute',
+        'telNr' => '8655555',
+        'amzius' => '21',
+        'lytis' => 'moteris'
+    ],
+    [
+        'vardas' => 'Bendzeminas',
+        'pavarde' => 'Benas',
+        'telNr' => '86666666',
+        'amzius' => '66',
+        'lytis' => 'vyras'
+    ],
+    [
+        'vardas' => 'Anastasija',
+        'pavarde' => 'MaistaRija',
+        'telNr' => '869696969',
+        'amzius' => '18',
+        'lytis' => 'moteris'
+    ],
+    [
+        'vardas' => 'Saules',
+        'pavarde' => 'Sunus',
+        'telNr' => '02',
+        'amzius' => '*9',
+        'lytis' => 'vyras'
+    ],
+    [
+        'vardas' => 'Vardenis',
+        'pavarde' => 'Pavardenis',
+        'telNr' => '02@one.lt',
+        'amzius' => '18',
+        'lytis' => 'vyras'
+    ],
+    [
+        'vardas' => 'Dzonas',
+        'pavarde' => 'Storas',
+        'telNr' => '1428',
+        'amzius' => '27',
+        'lytis' => 'vyras'
+    ]
 
+];
+$array = [
+    '44' => 'UK (+44)',
+    '1' => 'USA (+1)',
+    '213' => 'Algeria (+213)',
+    '376' => 'Andorra (+376)',
+    '244' => 'Angola (+244)',
+    '1264' => 'Anguilla (+1264)',
+    '1268' => 'Antigua & Barbuda (+1268)',
+    '54' => 'Argentina (+54)',
+    '374' => 'Armenia (+374)',
+    '297' => 'Aruba (+297)',
+    '61' => 'Australia (+61)',
+    '43' => 'Austria (+43)',
+    '994' => 'Azerbaijan (+994)',
+    '1242' => 'Bahamas (+1242)',
+    '973' => 'Bahrain (+973)',
+    '880' => 'Bangladesh (+880)',
+    '1246' => 'Barbados (+1246)',
+    '375' => 'Belarus (+375)',
+    '32' => 'Belgium (+32)',
+    '501' => 'Belize (+501)',
+    '229' => 'Benin (+229)',
+    '1441' => 'Bermuda (+1441)',
+    '975' => 'Bhutan (+975)',
+    '591' => 'Bolivia (+591)',
+    '387' => 'Bosnia Herzegovina (+387)',
+    '267' => 'Botswana (+267)',
+    '55' => 'Brazil (+55)',
+    '673' => 'Brunei (+673)',
+    '359' => 'Bulgaria (+359)',
+    '226' => 'Burkina Faso (+226)',
+    '257' => 'Burundi (+257)',
+    '855' => 'Cambodia (+855)',
+    '237' => 'Cameroon (+237)',
+    '1' => 'Canada (+1)',
+    '238' => 'Cape Verde Islands (+238)',
+    '1345' => 'Cayman Islands (+1345)',
+    '236' => 'Central African Republic (+236)',
+    '56' => 'Chile (+56)',
+    '86' => 'China (+86)',
+    '57' => 'Colombia (+57)',
+    '269' => 'Comoros (+269)',
+    '242' => 'Congo (+242)',
+    '682' => 'Cook Islands (+682)',
+    '506' => 'Costa Rica (+506)',
+    '385' => 'Croatia (+385)',
+    '53' => 'Cuba (+53)',
+    '90392' => 'Cyprus North (+90392)',
+    '357' => 'Cyprus South (+357)',
+    '42' => 'Czech Republic (+42)',
+    '45' => 'Denmark (+45)',
+    '253' => 'Djibouti (+253)',
+    '1809' => 'Dominica (+1809)',
+    '1809' => 'Dominican Republic (+1809)',
+    '593' => 'Ecuador (+593)',
+    '20' => 'Egypt (+20)',
+    '503' => 'El Salvador (+503)',
+    '240' => 'Equatorial Guinea (+240)',
+    '291' => 'Eritrea (+291)',
+    '372' => 'Estonia (+372)',
+    '251' => 'Ethiopia (+251)',
+    '500' => 'Falkland Islands (+500)',
+    '298' => 'Faroe Islands (+298)',
+    '679' => 'Fiji (+679)',
+    '358' => 'Finland (+358)',
+    '33' => 'France (+33)',
+    '594' => 'French Guiana (+594)',
+    '689' => 'French Polynesia (+689)',
+    '241' => 'Gabon (+241)',
+    '220' => 'Gambia (+220)',
+    '7880' => 'Georgia (+7880)',
+    '49' => 'Germany (+49)',
+    '233' => 'Ghana (+233)',
+    '350' => 'Gibraltar (+350)',
+    '30' => 'Greece (+30)',
+    '299' => 'Greenland (+299)',
+    '1473' => 'Grenada (+1473)',
+    '590' => 'Guadeloupe (+590)',
+    '671' => 'Guam (+671)',
+    '502' => 'Guatemala (+502)',
+    '224' => 'Guinea (+224)',
+    '245' => 'Guinea - Bissau (+245)',
+    '592' => 'Guyana (+592)',
+    '509' => 'Haiti (+509)',
+    '504' => 'Honduras (+504)',
+    '852' => 'Hong Kong (+852)',
+    '36' => 'Hungary (+36)',
+    '354' => 'Iceland (+354)',
+    '91' => 'India (+91)',
+    '62' => 'Indonesia (+62)',
+    '98' => 'Iran (+98)',
+    '964' => 'Iraq (+964)',
+    '353' => 'Ireland (+353)',
+    '972' => 'Israel (+972)',
+    '39' => 'Italy (+39)',
+    '1876' => 'Jamaica (+1876)',
+    '81' => 'Japan (+81)',
+    '962' => 'Jordan (+962)',
+    '7' => 'Kazakhstan (+7)',
+    '254' => 'Kenya (+254)',
+    '686' => 'Kiribati (+686)',
+    '850' => 'Korea North (+850)',
+    '82' => 'Korea South (+82)',
+    '965' => 'Kuwait (+965)',
+    '996' => 'Kyrgyzstan (+996)',
+    '856' => 'Laos (+856)',
+    '371' => 'Latvia (+371)',
+    '961' => 'Lebanon (+961)',
+    '266' => 'Lesotho (+266)',
+    '231' => 'Liberia (+231)',
+    '218' => 'Libya (+218)',
+    '417' => 'Liechtenstein (+417)',
+    '370' => 'Lithuania (+370)',
+    '352' => 'Luxembourg (+352)',
+    '853' => 'Macao (+853)',
+    '389' => 'Macedonia (+389)',
+    '261' => 'Madagascar (+261)',
+    '265' => 'Malawi (+265)',
+    '60' => 'Malaysia (+60)',
+    '960' => 'Maldives (+960)',
+    '223' => 'Mali (+223)',
+    '356' => 'Malta (+356)',
+    '692' => 'Marshall Islands (+692)',
+    '596' => 'Martinique (+596)',
+    '222' => 'Mauritania (+222)',
+    '269' => 'Mayotte (+269)',
+    '52' => 'Mexico (+52)',
+    '691' => 'Micronesia (+691)',
+    '373' => 'Moldova (+373)',
+    '377' => 'Monaco (+377)',
+    '976' => 'Mongolia (+976)',
+    '1664' => 'Montserrat (+1664)',
+    '212' => 'Morocco (+212)',
+    '258' => 'Mozambique (+258)',
+    '95' => 'Myanmar (+95)',
+    '264' => 'Namibia (+264)',
+    '674' => 'Nauru (+674)',
+    '977' => 'Nepal (+977)',
+    '31' => 'Netherlands (+31)',
+    '687' => 'New Caledonia (+687)',
+    '64' => 'New Zealand (+64)',
+    '505' => 'Nicaragua (+505)',
+    '227' => 'Niger (+227)',
+    '234' => 'Nigeria (+234)',
+    '683' => 'Niue (+683)',
+    '672' => 'Norfolk Islands (+672)',
+    '670' => 'Northern Marianas (+670)',
+    '47' => 'Norway (+47)',
+    '968' => 'Oman (+968)',
+    '680' => 'Palau (+680)',
+    '507' => 'Panama (+507)',
+    '675' => 'Papua New Guinea (+675)',
+    '595' => 'Paraguay (+595)',
+    '51' => 'Peru (+51)',
+    '63' => 'Philippines (+63)',
+    '48' => 'Poland (+48)',
+    '351' => 'Portugal (+351)',
+    '1787' => 'Puerto Rico (+1787)',
+    '974' => 'Qatar (+974)',
+    '262' => 'Reunion (+262)',
+    '40' => 'Romania (+40)',
+    '7' => 'Russia (+7)',
+    '250' => 'Rwanda (+250)',
+    '378' => 'San Marino (+378)',
+    '239' => 'Sao Tome & Principe (+239)',
+    '966' => 'Saudi Arabia (+966)',
+    '221' => 'Senegal (+221)',
+    '381' => 'Serbia (+381)',
+    '248' => 'Seychelles (+248)',
+    '232' => 'Sierra Leone (+232)',
+    '65' => 'Singapore (+65)',
+    '421' => 'Slovak Republic (+421)',
+    '386' => 'Slovenia (+386)',
+    '677' => 'Solomon Islands (+677)',
+    '252' => 'Somalia (+252)',
+    '27' => 'South Africa (+27)',
+    '34' => 'Spain (+34)',
+    '94' => 'Sri Lanka (+94)',
+    '290' => 'St. Helena (+290)',
+    '1869' => 'St. Kitts (+1869)',
+    '1758' => 'St. Lucia (+1758)',
+    '249' => 'Sudan (+249)',
+    '597' => 'Suriname (+597)',
+    '268' => 'Swaziland (+268)',
+    '46' => 'Sweden (+46)',
+    '41' => 'Switzerland (+41)',
+    '963' => 'Syria (+963)',
+    '886' => 'Taiwan (+886)',
+    '7' => 'Tajikstan (+7)',
+    '66' => 'Thailand (+66)',
+    '228' => 'Togo (+228)',
+    '676' => 'Tonga (+676)',
+    '1868' => 'Trinidad & Tobago (+1868)',
+    '216' => 'Tunisia (+216)',
+    '90' => 'Turkey (+90)',
+    '7' => 'Turkmenistan (+7)',
+    '993' => 'Turkmenistan (+993)',
+    '1649' => 'Turks & Caicos Islands (+1649)',
+    '688' => 'Tuvalu (+688)',
+    '256' => 'Uganda (+256)',
+    '380' => 'Ukraine (+380)',
+    '971' => 'United Arab Emirates (+971)',
+    '598' => 'Uruguay (+598)',
+    '7' => 'Uzbekistan (+7)',
+    '678' => 'Vanuatu (+678)',
+    '379' => 'Vatican City (+379)',
+    '58' => 'Venezuela (+58)',
+    '84' => 'Vietnam (+84)',
+    '84' => 'Virgin Islands - British (+1284)',
+    '84' => 'Virgin Islands - US (+1340)',
+    '681' => 'Wallis & Futuna (+681)',
+    '969' => 'Yemen (North)(+969)',
+    '967' => 'Yemen (South)(+967)',
+    '260' => 'Zambia (+260)',
+    '263' => 'Zimbabwe (+263)',
+];
+//foreach ($vartotojai as $item) {
+//    var_dump($item);
+//};
+$weekTemp = [
+    'monday' => rand(-22,28),
+    'tuesday' => rand(-22,28),
+    'wednesday' => rand(-22,28),
+    'thursday' => rand(-22,28),
+    'friday' => rand(-22,28),
+    'saturday' => rand(-22,28),
+    'sunday' => rand(-22,28),
+];
 
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Boom in <?php print 60-date("s") ;?></title>
-    <style>
-        .center {
-            display: flex;
-            flex-wrap: wrap;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%,-50%);
-            position: fixed;
-        }
-        .topic {
-            width: 100%;
-        }
-    </style>
+    <title>Document</title>
 </head>
-<body style="display: flex; text-align: center">
+<body>
 
-<div class="center">
-    <div class="topic">
-        <h3>vidutiniškai buteliukų išgeria per savaitę: <?php print $month ;?></h3>
-        <h5>modal <?php print $modalOfMonth ;?></h3></h5>
-        <h5>dalmuo <?php print $dalmuo ;?></h5>
-    </div>
+<table>
+<?php foreach ($weekTemp as $key => $item): ?>
 
-    <?php for($x = 0; $x <= $dalmuo; $x++) {
-        print '<div><img src="images/deze.jpg" width="120px" alt=""></div>';
-    };?>
-    <?php for($x = 0; $x <= $modalOfMonth; $x++) {
-        if ($x == 0){
-            null;
-        }else{
-            print '<div><img src="images/corona.jpg" width="120px" alt=""></div>';
-        }
-
-    };?>
+<th style="display: inline"><?php print $key;?></th>
+<td><?php print $item;?></td>
+<td><div style="height: 100px; width: 9px; background-color: red;border-radius: 3px">
+        <div style="height: <?php print 100-(44+($item*2));?>px; width: 9px; background-color: blue; border-radius: 3px 3px 0 0 "></div>
+    </div></td>
 
 
-</div>
+
+
+<?php endforeach;?>
+</table>
+
+<select name="phones" id="phones">
+<?php foreach ($array as $key => $item): ?>
+
+    <option value="<?php print $key;?>"><?php print $item;?></option>
+
+<?php endforeach;?>
+</select>
+<input type="text">
+<table>
+<?php foreach ($vartotojai as $item): ?>
+
+    <tr><th><?php print $item['vardas'] . ' ' .  $item['pavarde'];?></th></tr>
+    <tr><td><?php print $item['amzius'];?></td></tr>
+    <tr><td><?php print $item['telNr'];?></td></tr>
+    <tr><td><?php print $item['lytis'];?></td></tr>
+
+<?php endforeach;?>
+</table>
 
 
 
