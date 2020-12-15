92c306812b69c8c6cc2815cbe711c0f8dd4a0a70
edmundas3945 <74895998+edmundas3945@users.noreply.github.com>
2020-12-15 14:56:30 +0200

    taikinys su taskais #3

--- index.php
+++ index.php
@@ -8,12 +8,9 @@ date_default_timezone_set('Europe/Vilnius');
 //$sec = "1";
 //header("Refresh: $sec; url=$page");
 
-$shootX = rand(1,499);
-$shootY = rand(1,499);
-$targetX = rand(1,499);
-$targetY = rand(1,499);
-$paklaidaX = $shootX - $targetX;
-$paklaidaY = $shootY - $targetY;
+$shootX = rand(1, 599);
+$shootY = rand(1, 599);
+
 
 $thicknessLine = '3px'
 
@@ -33,111 +30,89 @@ $thicknessLine = '3px'
             position: relative;
             text-align: center;
         }
+
         .taikinys {
-            width: 500px;
-            height: 500px;
-            background-color: cadetblue;
-            border-radius: 50%;
+            width: 600px;
+            height: 600px;
+            background-image: url("images/taikinys.png");
+            background-size: cover;
             position: absolute;
             top: 390px;
             left: 50%;
             transform: translate(-50%, -50%);
             display: flex;
+            align-content: center;
+            justify-content: center;
         }
-        .target{
-            width: 21px;
-            height: 21px;
-            position: absolute;
-            top: <?php print $targetY ;?>px;
-            left: <?php print $targetX ;?>px;
+
+        .center {
+
             transform: translate(-50%, -50%);
-        }
-        .line1 {
-            width: 100%;
-            height: <?= $thicknessLine?>;
+            width: 300px;
+            height: 300px;
+            background-color: red;
             top: 50%;
-            left: -51%;
-            transform: translate(0,-50%);
-            background-color: lawngreen;
-            position: absolute;
-        }
-        .line2{
-            height: 100%;
-            width: <?= $thicknessLine?>;
             left: 50%;
-            top: -1%;
-            transform: translate(-50%,-50%);
-            background-color: lawngreen;
-            position: absolute;
         }
-        .line3 {
-            width: 100%;
-            height: <?= $thicknessLine?>;
-            top: 50%;
-            left: 51%;
-            transform: translate(0,-50%);
-            background-color: lawngreen;
-            position: absolute;
-        }
-        .line4{
-            height: 100%;
-            width: <?= $thicknessLine?>;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%,0);
-            background-color: lawngreen;
-            position: absolute;
-        }
-        .shoot{
+
+        .shoot {
             width: 21px;
             height: 21px;
             position: absolute;
             top: <?php print $shootY ;?>px;
             left: <?php print $shootX;?>px;
+            /*top: 504px;*/
+            /*left: 300px;*/
             transform: translate(-50%, -50%);
         }
+
         .redLine1 {
             width: 100%;
             height: <?= $thicknessLine?>;
             top: 50%;
             left: -51%;
-            transform: translate(0,-50%);
+            transform: translate(0, -50%);
             background-color: red;
             position: absolute;
         }
-        .redLine2{
+
+        .redLine2 {
             height: 100%;
             width: <?= $thicknessLine?>;
             left: 50%;
             top: -1%;
-            transform: translate(-50%,-50%);
+            transform: translate(-50%, -50%);
             background-color: red;
             position: absolute;
         }
+
         .redLine3 {
             width: 100%;
             height: <?= $thicknessLine?>;
             top: 50%;
             left: 51%;
-            transform: translate(0,-50%);
+            transform: translate(0, -50%);
             background-color: red;
             position: absolute;
         }
-        .redLine4{
+
+        .redLine4 {
             height: 100%;
             width: <?= $thicknessLine?>;
             left: 50%;
             top: 50%;
-            transform: translate(-50%,0);
+            transform: translate(-50%, 0);
             background-color: red;
             position: absolute;
         }
+
         .title {
             display: flex;
             text-align: center;
             justify-content: center;
         }
-        p{
+
+        p {
             color: blue;
             margin: 0;
         }
@@ -147,12 +122,7 @@ $thicknessLine = '3px'
 
 
 <div class="taikinys">
-    <div class="target">
-        <div class="line1"></div>
-        <div class="line2"></div>
-        <div class="line3"></div>
-        <div class="line4"></div>
-    </div>
+
     <div class="shoot">
         <div class="redLine1"></div>
         <div class="redLine2"></div>
@@ -160,11 +130,15 @@ $thicknessLine = '3px'
         <div class="redLine4"></div>
     </div>
 </div>
-<div class="title">mistake x:<p><?= abs($paklaidaX);?></p>,  y:<p><?= abs($paklaidaY);?></p></div>
-<div> target coordinates x: <?= $targetX;?>, y: <?= $targetY;?></div>
-<div> shoot coordinates x: <?= $shootX;?>, y: <?= $shootY;?></div>
-
-
+<div class="title">surinkta tasku: <p>
+        <?php if ($shootY > 177 && $shootY < 423 && $shootX > 177 && $shootX < 423): ?>
+            <?= '10' ?>
+        <?php elseif ($shootY > 96 && $shootY < 504 && $shootX > 96 && $shootX < 504): ?>
+            <?= '5' ?>
+        <?php elseif ($shootY > 0 && $shootY < 600 && $shootX > 0 && $shootX < 600): ?>
+            <?= '1' ?>
+        <?php endif ?>
+    </p></div>
 
 
 </body>
